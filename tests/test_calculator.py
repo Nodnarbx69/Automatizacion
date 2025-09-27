a6475b33d04c3c48b48b89f25de32034b1e2047e
Nodnarbx69 <branndonmaldonado@gmail.com>
2025-09-27 10:40:52 -0500

    Mi primer envio

---
+++ tests/test_calculator.py
@@ -0,0 +1,12 @@
+import unittest
+from src.calculator import suma, resta
+
+class TestCalculator(unittest.TestCase):
+    def test_suma(self):
+        self.assertEqual(suma(2,3),5)
+
+    def test_resta(self):
+        self.assertEqual(resta(5,3),2)
+
+if __name__ == '_main_':
+    unittest.main()
